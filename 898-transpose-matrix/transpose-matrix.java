80b1d0263c0cd80c7e582d523157cdc96d238e94
kunalrawat01 <rawatkunal625@gmail.com>
2026-03-04 21:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (45.42%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -3,12 +3,15 @@ class Solution {
         int rows = matrix.length;
         int cols = matrix[0].length;
         
-        // Create a new matrix with swapped dimensions
+
+        
         int[][] res = new int[cols][rows];
         
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                // Transpose: swap indices
+    
+
+
                 res[j][i] = matrix[i][j];
             }
         }
