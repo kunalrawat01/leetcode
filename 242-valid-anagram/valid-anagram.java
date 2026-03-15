dd4e71bca0cbf28785f9bb62169bbbbfa06b7d93
kunalrawat01 <rawatkunal625@gmail.com>
2026-03-15 23:00:08 +0530

    Time: 4 ms (81.45%) | Memory: 46.4 MB (44.72%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }else{
+            char[] sArr = s.toCharArray();
+            char[] tArr = t.toCharArray();
+
+
+            Arrays.sort(sArr);
+            Arrays.sort(tArr);
+          
+        for(int i=0;i<s.length();i++){
+             if (sArr[i] != tArr[i]){
+                return false;
+             }
+
+            }
+            return true;
+        }
+    }
+}
