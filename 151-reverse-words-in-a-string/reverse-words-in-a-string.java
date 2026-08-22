624a201074f60230ee0ce3ce6d85df09713c35d8
kunalrawat01 <rawatkunal625@gmail.com>
2026-08-22 18:10:16 +0530

    Time: 10 ms (17.51%) | Memory: 47 MB (10.43%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,11 +1,13 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
-        StringBuilder sb = new StringBuilder();
-        for (int i = words.length - 1; i >= 0; i--) {
-            sb.append(words[i]);
-            if (i > 0) sb.append(" ");
+        String[] word = s.trim().split("\\s+");
+        String ans = "";
+        for(int i = word.length - 1; i >= 0; i--) {
+            ans += word[i];
+            if(i != 0) {
+                ans += " ";
+            }
         }
-        return sb.toString();
+        return ans;
     }
 }
