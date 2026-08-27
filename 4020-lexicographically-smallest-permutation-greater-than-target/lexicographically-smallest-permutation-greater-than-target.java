c511afa3e69150cd8dee3cc010bcf2dcdc9d89bf
kunalrawat01 <rawatkunal625@gmail.com>
2026-08-27 21:37:48 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (77.42%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
@@ -1,54 +1,77 @@
 class Solution {
     public String lexGreaterPermutation(String s, String target) {
-        int[] count = new int[26];
+
+        int[] freq = new int[26];
+
         for (char c : s.toCharArray())
-            count[c - 'a']++;
+            freq[c - 'a']++;
+
         char[] ans = target.toCharArray();
-        for (int i = 0; i < target.length(); i++) {
+
+        int i = 0;
+
+        // Match target as much as possible
+        while (i < target.length()) {
+
             int x = target.charAt(i) - 'a';
-            if (count[x] > 0) {
-                count[x]--;
-            } else {
-                for (int j = i; j >= 0; j--) {
-                    if (j < i)
-                        count[ans[j] - 'a']++;
-                    int cur = target.charAt(j) - 'a';
-                    for (int k = cur + 1; k < 26; k++) {
-                        if (count[k] > 0) {
-                            ans[j] = (char) ('a' + k);
-                            count[k]--;
-                            int p = j + 1;
-                            for (int c = 0; c < 26; c++) {
-                                while (count[c] > 0) {
-                                    ans[p++] = (char) ('a' + c);
-                                    count[c]--;
-                                }
-                            }
-                            return new String(ans);
-                        }
-                    }
-                }
-                return "";
+
+            // Use the same character
+            if (freq[x] > 0) {
+                freq[x]--;
+                i++;
+                continue;
             }
-        }
-        for (int i = target.length() - 1; i >= 0; i--) {
-            count[ans[i] - 'a']++;
-            int cur = ans[i] - 'a';
-            for (int k = cur + 1; k < 26; k++) {
-                if (count[k] > 0) {
-                    ans[i] = (char) ('a' + k);
-                    count[k]--;
-                    int p = i + 1;
-                    for (int c = 0; c < 26; c++) {
-                        while (count[c] > 0) {
-                            ans[p++] = (char) ('a' + c);
-                            count[c]--;
-                        }
-                    }
-                    return new String(ans);
+
+            // Cannot use target[i].
+            // Try the smallest character greater than target[i].
+            for (int c = x + 1; c < 26; c++) {
+
+                if (freq[c] > 0) {
+                    ans[i] = (char) ('a' + c);
+                    freq[c]--;
+
+                    return build(ans, i + 1, freq);
                 }
             }
+
+            // Cannot make it greater here.
+            break;
         }
+
+        // Backtrack
+        for (int j = i - 1; j >= 0; j--) {
+
+            // Return the character used at j
+            freq[target.charAt(j) - 'a']++;
+
+            int x = target.charAt(j) - 'a';
+
+            // Find the smallest character greater than target[j]
+            for (int c = x + 1; c < 26; c++) {
+
+                if (freq[c] > 0) {
+                    ans[j] = (char) ('a' + c);
+                    freq[c]--;
+
+                    return build(ans, j + 1, freq);
+                }
+            }
+        }
+
         return "";
+    }
+
+    private String build(char[] ans, int start, int[] freq) {
+
+        int p = start;
+
+        for (int c = 0; c < 26; c++) {
+            while (freq[c] > 0) {
+                ans[p++] = (char) ('a' + c);
+                freq[c]--;
+            }
+        }
+
+        return new String(ans);
     }
 }
