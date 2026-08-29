da65854025bb5c7b336b35604364b3ecfddf0ec1
kunalrawat01 <rawatkunal625@gmail.com>
2026-08-29 14:35:43 +0530

    Time: 5 ms (12.19%) | Memory: 268.9 MB (6.69%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,5 +1,8 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        if(nums.length==0){
+            return;
+        }
       k = k % nums.length;
       reverse(nums,0,nums.length-1);
       reverse(nums,0,k-1);
