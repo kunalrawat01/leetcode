8f80077630844f8dcc96076a5bb172a33c4056fb
kunalrawat01 <rawatkunal625@gmail.com>
2026-08-31 22:18:20 +0530

    Time: 2 ms (99.30%) | Memory: 44.2 MB (83.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        String reversed = new StringBuilder(s).reverse().toString();
-        return s.equals(reversed);
+         int left = 0;
+         int right = s.length()-1;
+         while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+         }
+         return true;
     }
 }
