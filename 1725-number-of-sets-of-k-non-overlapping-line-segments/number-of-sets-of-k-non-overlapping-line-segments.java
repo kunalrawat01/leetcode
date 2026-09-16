df9b848167ed9bee4c3bbeb479b06904d2957dff
kunalrawat01 <rawatkunal625@gmail.com>
2026-09-16 20:42:50 +0530

    Time: 1 ms (95.48%) | Memory: 42.4 MB (82.58%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,21 +1,36 @@
 class Solution {
-    private static final int MOD = (int) 1e9 + 7;
+    private static final int MOD = (int)1e9 + 7;
 
     public int numberOfSets(int n, int k) {
-        int[][] f = new int[n + 1][k + 1];
-        int[][] g = new int[n + 1][k + 1];
-        f[1][0] = 1;
+        int size = n + k;
+        long[] fact = new long[size + 1];
+        long[] invFact = new long[size + 1];
 
-        for (int i = 2; i <= n; ++i) {
-            for (int j = 0; j <= k; ++j) {
-                f[i][j] = (f[i - 1][j] + g[i - 1][j]) % MOD;
-                g[i][j] = g[i - 1][j];
-                if (j > 0) {
-                    g[i][j] = (g[i][j] + f[i - 1][j - 1]) % MOD;
-                    g[i][j] = (g[i][j] + g[i - 1][j - 1]) % MOD;
-                }
-            }
+        fact[0] = 1;
+        for (int i = 1; i <= size; i++) {
+            fact[i] = fact[i - 1] * i % MOD;
         }
-        return (f[n][k] + g[n][k]) % MOD;
+
+        invFact[size] = pow(fact[size], MOD - 2);
+        for (int i = size; i > 0; i--) {
+            invFact[i - 1] = invFact[i] * i % MOD;
+        }
+
+        return (int) nCr(n + k - 1, 2 * k, fact, invFact);
+    }
+
+    private long nCr(int a, int b, long[] fact, long[] invFact) {
+        if (b < 0 || b > a) return 0;
+        return fact[a] * invFact[b] % MOD * invFact[a - b] % MOD;
+    }
+
+    private long pow(long base, int exp) {
+        long res = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) res = res * base % MOD;
+            base = base * base % MOD;
+            exp >>= 1;
+        }
+        return res;
     }
 }
